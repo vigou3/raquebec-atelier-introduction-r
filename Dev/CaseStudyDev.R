fe620492f7037b56101c5c19cb50fcf80f9821ac
Samuel Cabral Cruz <samuelcabralcruz@gmail.com>
2017-05-11 13:31:06 -0400

    test branch

--- Dev/CaseStudyDev.R
+++ Dev/CaseStudyDev.R
@@ -509,7 +509,6 @@ weightsError <- pnorm((x[,3]-0.5)*sqrt(12))*sd(weights)*weightsTarifParamA
 weightsPriceFinal <- weightsPrice + weightsError
 cbind(weights,weightsPriceFinal)
 
-
 #### Question 5 ####
 
 
