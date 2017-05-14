2432820ca350ff826bd0e3157fdf243799eec2cf
davebulaval <freresdarme@hotmail.com>
2017-05-14 19:48:00 -0400

    Ajout question 6 Ajout fichier R test_dave

--- Dev/CaseStudyDev.R
+++ Dev/CaseStudyDev.R
@@ -542,7 +542,7 @@ gamModel <- optim(c(1,1), minGamma)
 
 minLN <- function(par) 
 {
-     -sum(dlnorm(compData$Prix, par[1], par[2], log = TRUE))
+     -sum(dlnorm(compData$weight, par[1], par[2], log = TRUE))
 }
 lnModel <- optim(c(1,1), minLN)
 
@@ -584,7 +584,7 @@ min(c(gamModel$value, lnModel$value))
 
 library("MASS")
 
-fit.ln <- fitdistr(compData$Prix, "lognormal")
+fit.ln <- fitdistr(compData$weight, "lognormal")
 fit.ln
 
 fit.gamma <- fitdistr(compData$Prix, "gamma")
@@ -592,6 +592,69 @@ fit.gamma
 
 
 #### Question 6 ####
+
+theurl <- getURL("https://raw.githubusercontent.com/vigou3/raquebec-intro/master/Statement/CaseStudyStatement.html",.opts = list(ssl.verifypeer = FALSE) )
+tables <- readHTMLTable(theurl)
+lambdaTable <- as.data.frame(tables[1], stringsAsFactors = TRUE)
+colnames(lambdaTable) <- c("Month","Average")
+lambdaTable
+
+
+simulFunction <- function(nSimul,  paramDensity, paramWeight)
+{
+     densityArrival <- as.numeric(as.character(arrivalIndexTable[,2])) / sum(as.numeric(as.character(arrivalIndexTable[,2])))
+     shippingYear <- replicate(nSimul, sum(sapply(paramDensity, function(paramDensity) rpois(1, paramDensity))))
+     weightSimul <- sapply(1:nSimul, function(k) replicate(shippingYear[k], rlnorm(1, paramWeight[1], paramWeight[2])))
+     destIATA <- sapply(1:nSimul, function(k) replicate(shippingYear[k], sample(x = as.character(arrivalIndexTable[,1]), size = 1, prob = densityArrival)))
+     price <- matrix(nrow = nSimul, ncol = max(shippingYear))
+     for (i in 1:nSimul)
+     {
+          for (j in 1:shippingYear[i])
+          {
+               price[i, j] <-  ifelse(is(try(shippingCost("YUL", destIATA[[i]][j], weightSimul[[i]][j]), silent = TRUE), "try-error") == TRUE, NA, shippingCost("YUL", destIATA[[i]][j], weightSimul[[i]][j])$price)
+          }
+     }
+     # Return List
+     shippingYearList <- list()
+     shippingYearList$density <- density
+     shippingYearList$nSimul <- nSimul
+     shippingYearList$paramDensity <- paramDensity
+     shippingYearList$paramWeight <- paramWeight
+     shippingYearList$numberShipYear <- shippingYear
+     shippingYearList$weight <- weightSimul
+     shippingYearList$desIATA <- destIATA
+     shippingYearList$Price <- price
+     shippingYearList
+}
+lnModelsSimul <- simulFunction(2, paramDensity = as.numeric(as.character(lambdaTable$Average)), paramWeight = lnModel$par)
+lnModelsSimul[[1]][1]
+lnModelsSimul$numberShipYear
+max(lnModelsSimul$Price)
+
+View(lnModelsSimul$Price)
+
+
+
+
+# test
+
+#alternative
+#shippingYear <- replicate(sum(sapply(as.numeric(as.character(lambdaTable$Average)), function(lambda) rpois(1, lambda))), n = nSimul)
+
+# Calcul des prix
+priceSimul <- function(modelSimulated)
+{
+     price <- numeric(modelSimulated$)
+     is(try(sapply(1:lnModelsSimul$nSimul, function(simulI) sapply(1:lnModelsSimul$numberShipYear[simulI], 
+                                                                   function(parcelJ) shippingCost("YUL", lnModelsSimul$desIATA[[simulI]][parcelJ], lnModelsSimul$weight[[simulI]][parcelJ] ))), silent = TRUE), "try-error")
+     
+}
+     
+is(try(sapply(1:lnModelsSimul$nSimul, function(simulI) sapply(1:lnModelsSimul$numberShipYear[simulI], 
+                                                       function(parcelJ) shippingCost("YUL", lnModelsSimul$desIATA[[simulI]][parcelJ], lnModelsSimul$weight[[simulI]][parcelJ] ))),silent = TRUE), "try-error")
+
+
+
 f<-function(x)
 {
   if(x < 0)
@@ -607,3 +670,4 @@ is(test,"try-error")
 
 test <- try(f(-2),silent=TRUE)
 is(test,"try-error")
+test
