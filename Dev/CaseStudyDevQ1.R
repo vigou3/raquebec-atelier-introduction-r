80cbcb427b9fd6d13d424ccd2d602c7b4be615f0
davebulaval <freresdarme@hotmail.com>
2017-05-17 22:38:54 -0400

    Trad Q1

--- Dev/CaseStudyDevQ1.R
+++ Dev/CaseStudyDevQ1.R
@@ -1,4 +1,8 @@
-# Code source Case Study R à Québec 2017
+#
+# This is the main code for the Case Study R à Québec 2017
+#
+# Author : David Beauchemin & Samuel Cabral Cruz
+#
 
 #### Setting working directory properly ####
 getwd()
@@ -8,12 +12,12 @@ set.seed(31459)
 
 #### Question 1 - Data extraction, processing, visualization and analysis ####
 
-# 1.1 - Database extraction of airports.dat, routes.dat and airlines.dat
+# 1.1 - Database extraction of airports.dat, routes.dat and airlines.dat.
 airports <- read.csv("https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat", header = FALSE, na.strings=c('\\N',''))
 routes <- read.csv("https://raw.githubusercontent.com/jpatokal/openflights/master/data/routes.dat", header = FALSE, na.strings=c('\\N',''))
 airlines <- read.csv("https://raw.githubusercontent.com/jpatokal/openflights/master/data/airlines.dat", header = FALSE, na.strings=c('\\N',''))
 
-# 1.2 - Coloumns names assignation  base on the information available on the website
+# 1.2 - Coloumns names assignation  base on the information available on the website.
 colnames(airports) <- c("airportID", "name", "city", "country", "IATA", "ICAO",
                         "latitude", "longitude", "altitude", "timezone", "DST",
                         "tzFormat","typeAirport","Source")
@@ -25,34 +29,30 @@ colnames(airlines) <- c("airlineID","name","alias","IATA","ICAO","Callsign","Cou
 # 1.3 - Keeping the Canada information of the dataset
 airportsCanada <- airports[airports$country=='Canada',]
 
-# 1.4 - Extraire des informations générales sur la distribution des variables présentent dans le jeu de données 
-# et vous informer sur la signification de ces dernières ainsi que sur les différentes modalités quelles peuvent 
-# prendre
+# 1.4 - Extraction of the genreral information about the distributions of the variables in the dataset and 
+# understanding of the signification of those variables and the different modalities they can take.
 
-#Voici différente fonction R qui vous permette de visualiser les données
+# Those are the principal R function to easely visualize information.
 View(airportsCanada)
 head(airportsCanada)
 summary(airportsCanada)
 
-nbAirportCity <- table(as.character(airportsCanada$city))   #We use as.character to convert the factor into a fixed caractor
+nbAirportCity <- table(airportsCanada$city)  
 (nbAirportCity <- sort(nbAirportCity,decreasing=TRUE))[1:10]
 
-# Filtrer la pertinence des données
-# 1.5 - Corriger les modalités des variables et faire une sélection des variables qui vous semble utiles 
-# pour le reste du traitement
-# Nous observons que les variables typeAirport et Source ne sont d'aucune utilité dans la situation présente
-# compte tenu que nous n'utilisons que l'information sur le transport par voies aériennes.
-# Un raisonnement similaire est applicable pour la variable country qui ne possèdera que la modalité Canada
+# The relevance of the data.
+# 1.5 - Correct the modalities of the variables and make a selection of those wo seem useful for the rest of the treatment. 
+# We observe that the variables typeAirport and Source are useless in our situation since we only use information on air transport. 
+# A similar reasoning is applicable for the country variable which will only have the modality Canada.
 airportsCanada <- subset(airportsCanada, select = -c(country, typeAirport, Source ))
 
 
 # As seen in the sumary, we dont have the IATA for 27 airports
 airportsCanada[is.na(airportsCanada$IATA),c("airportID","name","IATA","ICAO")]
 
-# Cependant, toutes ces aéroports possèdent un code ICAO bien défini ce qui permettra d'attribuer une valeur
-# par défaut. Une autre possibilité aurait été de simplement ignorer ces aéroports dans le reste de l'analyse.
-# Cette approximation ne semble pas trop contraignante compte tenu du fait que les trois derniers caractères
-# du code ICAO correspondent au code IATA dans 82% des cas.
+# The first option, is to simply ignore these airports in the rest of the analysis.
+# However, all these airports have a well-defined ICAO code which will allow a default value to be assigned.
+# Since 82% of the IACA is the last three caracters of the ICAO, we will simply use the derivate IATA from the ICAO.
 sum(airportsCanada$IATA==substr(airportsCanada$ICAO,2,4),na.rm = TRUE)/sum(!is.na(airportsCanada$IATA))
 
 # We are now able to fill the missing IATA and we delete the IACO since it's now useless
@@ -67,9 +67,9 @@ View(airportsCanada)
 missingTZ <- airportsCanada[is.na(airportsCanada$timezone),]
 
 # Since the TZ depend only on the geographical position, two options are available to us : 
-# 1) Deduce the information from other close airport
-# 2) Locate the real time zone by mapping tools
-# We will use the second option which may seem more complex but with the proper tools become more easy and accurate
+# 1) Deduce the information from other close airport;
+# 2) Locate the real time zone by mapping tools.
+# We will use the second option which may seem more complex but with the proper tools become more easy and accurate.
 
 # install.packages("sp")
 # install.packages("rgdal")
@@ -82,10 +82,9 @@ proj4string(sppts) <- CRS("+proj=longlat")
 sppts <- spTransform(sppts, proj4string(tz_world.shape))
 merged_tz <- cbind(unknown_tz,over(sppts,tz_world.shape))
 
-#Nous pouvons aussi remarquer que nous n'avons qu'une information dérivée de la province dans laquelle
-#est situé chaque aéroport via la ville. Dans le cas de l'application des taxes qui varie par province,
-#il sera donc indispensable de rendre cette donnée plus accessible. Nous procéderons encore une fois via
-#des techniques de cartographie afin d'extraire la province en fonction des coordonnées x et y
+# We can also note that we only have information derived from the province in which each airport is located with the city.
+# Since we want to apply taxes by province in our situation, we will need a better data to access this informaiton. 
+# We will again use mapping techniques to extract the province as a function of the x and y coordinates.
 prov_terr.shape <- readOGR(dsn=paste(path,"/Reference/prov_terr",sep=''),layer="gpr_000b11a_e")
 unknown_prov <- airportsCanada[,c("airportID","city","longitude","latitude")]
 sppts <- SpatialPoints(unknown_prov[,c("longitude","latitude")])
@@ -108,9 +107,8 @@ airportsCanada <- sqldf("
   order by a.airportID")
 airportsCanada <- data.frame(as.matrix(airportsCanada))
 
-# On retire timezone et DST car les données sont inutiles
-# On retire tzFormat car il s'Agit des données incomplet et on le remplace plus tard par tzMerged
-# On retire city qui ne sera plus utile puisque nous avons maintenant la province
+# Since the timezone, DST and city are now useless, we remove them from the dataset.
+# Plus, we withdraw tzFormat because it's incomplet and we will use the tzmerge data to replace will a complete data. 
 airportsCanada <- subset(airportsCanada, select = -c(timezone, DST, tzFormat, city ))
 summary(airportsCanada)
 
@@ -128,7 +126,7 @@ routesCanada <- sqldf("
                                  from airportsCanada)")
 routesCanada  <- data.frame(as.matrix(routesCanada ))
 
-# Produira le même résultat
+# This code will give the same result :
 # x <- routesCanada[!is.na(match(routesCanada$sourceAirportID,airportsCanada$airportID)) &
 #              !is.na(match(routesCanada$destinationAirportID,airportsCanada$airportID)),]
 # routesCanada <- routesCanada[!is.na(match(routesCanada$sourceAirport,airportsCanada$IATA)) &
@@ -148,17 +146,15 @@ routesCanada[is.na(routesCanada$airlineID),]
 unique(routesCanada$airlineID)
 unique(routesCanada[is.na(routesCanada$airlineID),]$airline)
 summary(routesCanada$stops)
-# Comme nous pouvons le remarquer, il n'y a que deux vols qui ne sont pas directs,
-# Pour des fins de simplification, nous allons considérer tous les vols comme étant 
-# des vols directs
-# De plus, la notion de codeshare ne sera pas utile compte tenu que la livraison de
-# de marchandise peut autant se faire par l'intermédiaire d'agence aérienne que par 
-# vol privé.
-# Nous pouvons ainsi nous débarasser de ces variables
+# As we can see, there are only two flights that are not direct.
+# For the sake of simplicity, we will consider all flights as direct flights.
+# Moreover, the notion of codeshare will not be useful since the delivery of 
+# merchandise can be done as much through an air agency as by private flight.
+# Then we get rid of these variables.
 routesCanada <- subset(routesCanada, select = -c(codeshare, stops))
 summary(routesCanada)
 
-# 1.6 - Créer une carte affichant les différents aéroports sur une carte du Canada
+# 1.6 - Create a map showing the different airports on a map of Canada.
 # install.packages("ggmap")
 library(ggmap)
 map <- get_map(location = 'Canada', zoom = 3)
@@ -167,7 +163,7 @@ lat <- as.numeric(paste(airportsCanada$latitude))
 airportsCoord <- as.data.frame(cbind(lon, lat))
 (mapPoints <- ggmap(map) + geom_point(data=airportsCoord,aes(lon,lat),alpha=0.5))
 
-# 1.7 - Créer une seconde carte montrant toutes les routes possibles entre ces différents aéroports
+# 1.7 - Create a second map showing all possible routes between these different airports.
 summary(routesCanada)
 summary(airportsCanada)
 routesCoord <- sqldf("
@@ -190,7 +186,7 @@ latEnd <- as.numeric(paste(routesCoord$destLat))
 routesCoord <- as.data.frame(cbind(lonBeg,latBeg,lonEnd,latEnd))
 (mapRoutes <- mapPoints + geom_segment(data=routesCoord,aes(x=lonBeg,y=latBeg,xend=lonEnd,yend=latEnd),alpha=0.5))
 
-# Calculer un indice d'achalandage des aéroports en fonction de la quantitée de routes en destination
+# Calculate an airport ridership index based on the number of incoming routes.
 arrivalFlights <- table(routesCanada$destinationAirport)
 departureFlights <- table(routesCanada$sourceAirport)
 totalFlights <- arrivalFlights + departureFlights
@@ -202,12 +198,12 @@ head(sort(totalFlights,decreasing = TRUE),n = 30)
 totalFlightsCDF <- ecdf(totalFlights)
 IATA <- names(totalFlights)
 
-# Tracer l'index
+# Index drawing
 curve(totalFlightsCDF(x-1),from = 0,to = 60,n = 100,
       xlab = "Nombre de routes par aéroport", 
       ylab = "CDF")
 
-# Calculer un indice combiné des deux derniers indices 
+# Calculate a combined index from the index
 combinedIndex <- round(totalFlights/max(totalFlights),3)
 combinedIndexTable <- data.frame(IATA,
                                  as.numeric(paste(totalFlights)),
@@ -224,7 +220,7 @@ airportsCanada <- sqldf("
   on a.IATA = b.IATA")
 airportsCanada <- data.frame(as.matrix(airportsCanada ))
 
-#1.11 - Créer des cartes permettant de visualiser ces indices grâce à un graphique à bulles
+#1.11 - Create maps to visualize these indices using a bubble graph
 lon <- as.numeric(paste(airportsCanada$longitude))
 lat <- as.numeric(paste(airportsCanada$latitude))
 size <- as.numeric(paste(airportsCanada$combinedIndex))
