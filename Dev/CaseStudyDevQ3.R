844e11fb1051f0bb3055f57bfc4a1a43815b10c5
Samuel Cabral Cruz <samuelcabralcruz@gmail.com>
2017-05-22 15:40:05 -0400

    In progress - Part 3 - plot and curve

--- Dev/CaseStudyDevQ3.R
+++ Dev/CaseStudyDevQ3.R
@@ -13,5 +13,19 @@ curve(shippingCost("YUL","YYZ",x)$price,0.01,50,xlab="weight (Kg)",
       ylab="price (CND $)",add=TRUE, col = "blue", lwd = 2)
 curve(shippingCost("YUL","YYC",x)$price,0.01,50,xlab="weight (Kg)",
       ylab="price (CND $)",add=TRUE, col = "purple", lwd = 2)
-text(x=c(45,45,45,45),y=c(50,110,140,175),c("YUL-YYZ","YUL-YQB","YUL-YVR","YUL-YYC"),adj = 0.5,cex = 0.75,font = 2,col = c("blue","black","red","purple"))
+text(x=c(25,25,25,25),y=c(50,90,140,175),c("YUL-YYZ","YUL-YQB","YUL-YVR","YUL-YYC"),adj = 0.5,cex = 0.75,font = 2,col = c("blue","black","red","purple"))
 
+
+fquad <- function(x,a=2,b=3,c=4)
+{
+  a*x**2+b*x+c
+}
+fquad(2)
+par(mfrow = c(2,2))
+plot(x <- seq(-10,10,10),fquad(x,2,3,4),type = "l",ylab = "fquad(x)",xlab = "x",main = "dx = 10")
+plot(x <- seq(-10,10,5),fquad(x,2,3,4),type = "l",ylab = "fquad(x)",xlab = "x",main = "dx = 5")
+plot(x <- seq(-10,10,2),fquad(x,2,3,4),type = "l",ylab = "fquad(x)",xlab = "x",main = "dx = 2")
+plot(x <- seq(-10,10),fquad(x,2,3,4),type = "l",ylab = "fquad(x)",xlab = "x",main = "dx = 1")
+
+par(mfrow = c(1,1))
+curve(fquad(x),from = -10,to = 10)
