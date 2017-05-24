bdb9b0ad278b0c84c6235072d9098e45dd44f0d1
David Beauchemin <freresdarme@hotmail.com>
2017-05-23 23:52:35 -0400

    modif airportModif

--- Dev/AirportModif.R
+++ Dev/AirportModif.R
@@ -12,7 +12,8 @@ setwd("..")    # Dont execute two times
 (path <- getwd())
 
 # Import of the original data
-airports <- read.csv("https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat", header = FALSE, na.strings=c("\\N",""), fileEncoding = "UTF-8")
+airports <- read.csv("https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat"
+                     , header = FALSE, na.strings=c("\\N",""), fileEncoding = "UTF-8")
 
 # Setting name
 colnames(airports) <- c("airportID", "name", "city", "country", "IATA", "ICAO",
@@ -26,38 +27,19 @@ airports$IATA[is.na(airports$IATA)] <- substr(airports$ICAO[is.na(airports$IATA)
 airports$IATA <- as.factor(airports$IATA)
 airports <- subset(airports, select = -ICAO)
 
-# install.packages("sp")
-# install.packages("rgdal")
-library(sp)
-library(rgdal)
-tz_world.shape <- readOGR(dsn=paste(path,"/Reference/tz_world",sep=""),layer="tz_world")
-unknown_tz <- airports[is.na(airports$tzFormat),c("airportID","name","longitude","latitude")]
-sppts <- SpatialPoints(unknown_tz[,c("longitude","latitude")])
-proj4string(sppts) <- CRS("+proj=longlat")
-sppts <- spTransform(sppts, proj4string(tz_world.shape))
-merged_tz <- cbind(unknown_tz,over(sppts,tz_world.shape))
 
-# install.packages("sqldf")
-library(sqldf)
-airports <- sqldf("
-                        select 
-                        a.*, 
-                        coalesce(a.tzFormat,b.TZID) as tzMerged
-                        from airports a 
-                        left join merged_tz b
-                        on a.airportID = b.airportID
-                        order by a.airportID")
-airports <- data.frame(as.matrix(airports))
 
-# Since the timezone, DST and city are now useless, we remove them from the dataset.
-# Plus, we withdraw tzFormat because it's incomplet and we will use the tzmerge data to replace will a complete data. 
-airports <- subset(airports, select = -tzFormat )
-summary(airports)
+# Adding the number of arrivalFlights for each airports
+routesCanada <- read.csv(paste(path,"/Reference/RoutesModif.csv",sep=""),  na.strings=c("\\N",""), 
+                         fileEncoding = "UTF-8", comment = "#")
 
-# install.packages("plyr")
-library(plyr)
-airports <- rename(airports, c("tzMerged"="tzFormat"))
-summary(airports)
+arrivalFlights <- table(routesCanada$destinationAirport)
+arrivalFlights <- data.frame(arrivalFlights)
+colnames(arrivalFlights) <- c("IATA","totalFlights")
+# On joint les données sur l'achalandage aux aéroports canadiens appropriés
+airports<- merge(airportsCanada, arrivalFlights, by.x = "IATA", by.y = "IATA")
+
+
 
 # We have fill all the missing tzFormat for the CaseStudyStudent document
 write.csv(airports, paste(path,"/Reference/AirportModif.csv",sep=""),row.names = FALSE, fileEncoding = "UTF-8", na = c("\\N",""))
